6d2c9a69e1e3c109c119ce55987c506dd5cbb96d
i526266 <ITler@users.noreply.github.com>
2023-04-06 15:55:50 +0200

    fix: remove deprecated GH opts handling

--- api/gh/gh.go
+++ api/gh/gh.go
@@ -29,47 +29,6 @@ type Queryable interface {
 	Query(context.Context, *github.Client) (interface{}, error)
 }
 
-// GetNewOpts provides [github.SearchOptions]
-// optionally based on existing search options and an API response.
-//
-// This facilitates setting proper search options for handling paging, properly,
-// and can be used in a do-while loop that encapsulates and accumulates API responses.
-//
-//	allRepos := []*github.Repository{}
-//	for opts = GetNewOpts(opts, nil); opts != nil; opts = GetNewOpts(opts, resp) {
-//		res, resp, err = gh.Search.Repositories(ctx, query, opts)
-//		if err != nil {
-//			return nil, err
-//		}
-//		allRepos = append(allRepos, res.Repositories...)
-//	}
-//
-// When opts and resp are provided, GetNewOpts will return nil if
-// there is no page left to query.
-func GetNewOpts(opts *github.SearchOptions, resp *github.Response) *github.SearchOptions {
-	currentPage := 0
-	if opts == nil {
-		opts = &github.SearchOptions{
-			Sort:      "",
-			Order:     "",
-			TextMatch: false,
-			ListOptions: github.ListOptions{
-				Page:    currentPage,
-				PerPage: PerPageResultsDefault,
-			},
-		}
-	}
-	if resp != nil {
-		if resp.LastPage > 0 {
-			opts.Page = resp.NextPage
-		} else {
-			return nil
-		}
-	}
-
-	return opts
-}
-
 // NewClient conveniently creates a client connection to the Github API
 // based on an already authenticated http client connection
 //
